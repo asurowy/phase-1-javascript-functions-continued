a8f931ed4f726ab0f8f3b1a931469eb42e3b6718
asurowy <101614024+asurowy@users.noreply.github.com>
2022-07-24 20:54:33 -0400

    commit tree 178e53724227f1fcfc14427bd6b8bddcde306c00

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun(); //This Saturday, I want to roller-skate! 
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+function wrapAdjective(initializedWith){
+let encouragingPromptFunction = function (adjective = "special"){
+return `You are ${initializedWith}${adjective}${initializedWith}!`;
+};
+return encouragingPromptFunction;
+}
+
+wrapAdjective("*")("a hard worker"); //=>
+wrapAdjective("||")("a dedicated programmer"); //=> 
